53e42f925b4fbe34efd3651952cb87d1c55fdb3d
Rory <p2628240@my365.dmu.ac.uk>
2023-11-30 14:24:26 +0000

    changes made

---
+++ app/src/HomePageView.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Telemetry;
+
+class HomePageView
+{
+    public function __construct() {}
+
+    public function __destruct() {}
+
+    public function createHomePageView($view, array $settings, $response): void
+    {
+        $landing_page = $settings['landing_page'];
+        $application_name = $settings['application_name'];
+        $css_path = $settings['css_path'];
+
+        $view->render(
+            $response,
+            'homepageform.hmtl.twig',
+            [
+                'css_path' => $css_path,
+                'landing_page' => $landing_page,
+                'action' => 'registeruser',
+                'method' => 'post',
+                'initial_input_box_value' => null,
+                'page_title' => $application_name,
+                'page_heading_1' => $application_name,
+                'page_heading_2' => 'New User Registration'
+            ]);
+    }
+
+}
--- app/src/RegisterUser.php
+++ app/src/RegisterUser.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace src;
 class RegisterUser
 {
 
