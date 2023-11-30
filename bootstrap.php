a3924302ad0d74ab8366a189ba66425384fb1eb1
Mak Duffy <P2635754@my365.dmu.ac.uk>
2023-11-30 13:31:46 +0000

    includes file

---
+++ bootstrap.php
@@ -0,0 +1,38 @@
+<?php
+
+declare (strict_types=1);
+
+use DI\Container;
+use Slim\Factory\AppFactory;
+
+
+if (session_start()) {
+
+    require 'vendor/autoload.php';
+
+    $base_dir = dirname(__DIR__);
+    $app_dir = $base_dir .'/Telemetry/app/';
+    $config_dir = $app_dir . 'config/';
+    $routes_dir =  $app_dir . 'routes/';
+
+    $container = new Container();
+
+    App::setContainer($container);
+
+    $app = AppFactory::create();
+
+    $app->setBasePath('/Telemetry');
+
+    $settings = require $config_dir . 'settings.php';
+    $settings($container, $app_dir);
+
+    $middleware = require $config_dir . 'middleware.php';
+    $middleware($app);
+
+    $dependencies = require $config_dir . 'dependencies.php';
+    $dependencies($container, $app);
+
+    require $routes_dir . 'routes.php';
+
+    $app->run();
+}
