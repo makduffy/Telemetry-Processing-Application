3fcca0a31edcc52cea98d0b4768155be843220fb
Rory <p2628240@my365.dmu.ac.uk>
2023-12-08 14:42:15 +0000

    removed homepage variables

--- app/src/views/HomePageView.php
+++ app/src/views/HomePageView.php
@@ -13,20 +13,7 @@ class HomePageView
         $application_name = $settings['application_name'];
         $css_path = $settings['css_path'];
 
-        $view->render(
-            $response,
-            'homepageform.html.twig',
-            [
-                'css_path' => $css_path,
-                'application_name' => $application_name,
-                'landing_page' => $landing_page,
-                'action' => 'storesessiondetails',
-                'initial_input_box_value' => null,
-                'page_heading_1' => 'Sessions Demonstration',
-                'page_heading_2' => 'Enter values for storage in a session',
-                'page_heading_3' => 'Select the type of session storage to be used',
 
-            ]);
     }
 
 }
