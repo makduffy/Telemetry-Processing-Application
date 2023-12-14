4cfddc3e024365f92379ede56f357285a8794cd7
Mak Duffy <P2635754@my365.dmu.ac.uk>
2023-12-14 19:44:21 +0000

    g

--- app/src/controllers/TelemetryController.php
+++ app/src/controllers/TelemetryController.php
@@ -11,9 +11,21 @@ class TelemetryController {
         $telemetry_view = $container->get('telemetryView');
         $telemetry_model = $container->get('telemetryModel');
         $soap_wrapper = $container->get('soapWrapper');
+        $database_wrapper = $container->get('databaseWrapper');
 
-        $telemetry_data = $telemetry_model->retrieveData($soap_wrapper, $settings);
-        $telemetry_view->showTelemetryPage($view, $settings, $response, $telemetry_data);
+        $telemetry_data = $telemetry_model->callTelemetryData($soap_wrapper, $settings);
+
+        $fan_data = $telemetry_model->filterArray($telemetry_data, 'fan');
+        $heater_data = $telemetry_model->filterArray($telemetry_data, 'heater');
+        $keypad_data = $telemetry_model->filterArray($telemetry_data, 'keypad');
+        $switch_data = $telemetry_model->filterArray($telemetry_data, 'switch');
+
+        $fan_data = $telemetry_model->sanitizeData($fan_data);
+        $heater_data = $telemetry_model->sanitizeData($heater_data);
+        $keypad_data = $telemetry_model->sanitizeData($keypad_data);
+        $switch_data = $telemetry_model->sanitizeData($switch_data);
+
+        $telemetry_view->showTelemetryPage($view, $settings, $response, $fan_data, $heater_data, $switch_data, $keypad_data);
 }
 
 }
